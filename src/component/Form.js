3bbcc0a4cbf96c99a6284e85130ae3447eaad367
tom <kotom320@gmail.com>
2020-03-21 17:51:59 +0900

    trying change

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,20 +1,66 @@
 import { create } from "domain";
 
 export class Form {
-    constructor(){
+    constructor(id,items){
         this.id = id;
-        this.element = null;
         this.items = items;
         create();
+        this.count = 1;
     }
 
     create(){
-        console.error('Override create method.');
+        this.survey = document.querySelector("#survey");
+        this.newForm = document.createElement('form');
+        this.newForm.name = 'form';
+        this.newForm.method ='post';
+        this.newForm.action = '#';
+        
+        this.preBtn = document.createElement('button');
+        this.preBtn.id = 'pre';
+        this.preBtn.innerText = '이전';
+
+        this.nextBtn = document.createElement('button');
+        this.nextBtn.id = 'next';
+        this.nextBtn.innerText = '다음';
+
+        this.btnSubmit = document.createElement('input');
+        this.btnSubmit.type = "submit";
+        this.btnSubmit.value = "확인";
+
+        this.title = document.createElement("h3");
+        this.title.innerHTML = items.subTitle;
+
+        this.newForm.appendChild(this.title);
+        renderSurvey(this.items);
+        this.newForm.appendChild(this.preBtn);
+        this.newForm.appendChild(this.nextBtn);
+        nextBtnAct();
+        preBtnAtc();
+
+        this.survey.appendChild(this.newForm);
     }
-    get getValue() {
-        return this.element.value;
+    renderSurvey(id,items){
+
     }
-    getElement() {
-        return this.element;
+    removeSurvey(id){
     }
+    nextBtnAct(){
+        this.nextBtn.addEventListener("click", ()=>{
+            this.removeSurvey();
+            
+        });
+    }
+    preBtnAtc(){
+        this.preBtn.addEventListener("click", ()=>{
+            this.removeSurvey();
+            this.renderSurvey();
+        });
+    }
+    
+
+    submit(){
+        
+        
+    }
+  
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import "./style.css";
 
 // import { DocumentSelectionExample } from "./component/document-selection-example";
 import { Survey } from "./component/survey"; 
+import { InputTextBox } from "./component/InputTextBox";
 const excute = () => {
   // const documentSelectionExample = new DocumentSelectionExample();
   const survey = new Survey();
+  // const inputText = new InputTextBox(1,{subTitle : "1.이름을 입력하세요",type : "text",name:"s1",value : [""]});
+
 };
 
 excute();
